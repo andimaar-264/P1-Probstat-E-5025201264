608867618651dad64bb44caf9ca1b1f369d2294b
andimaar-264 <80830860+andimaar-264@users.noreply.github.com>
2022-04-10 21:45:26 +0700

    Add files via upload

---
+++ No4.R
@@ -0,0 +1,29 @@
+#4 x=2, v=10
+
+#4a fungsi prob dan dist chisquare
+x <- 2
+v <- 10
+
+integral <- function(x)
+{
+  return (x^(5) * exp(1)^(-x))
+}
+
+temp <- integrate(integral, 0, Inf)
+
+z <- as.numeric(temp[1])
+
+fx <- function(input)
+{
+  return (1 / (z * 2^(1/2 * v)) * x^(v/2 - 1) * exp(1)^(-x/2) * input)
+}
+
+#4b histogram 100 data
+
+set.seed(1)
+r <- rnorm(1000)
+hist(fx(r[1:100]), main = "100 Data Histogram", breaks = 100, xlab = "Range")
+
+#4c rerata dan varian
+
+paste ("Rerata = ", v, " | Varian = ", v*2)
